ffb719cf161a4e30c2e77453fd80189f59873760
Tapan Kheni <tapankheni@Tapans-MacBook-Air.local>
2024-09-17 14:29:05 +0530

    second phase of meeting tracking component added

--- page/meeting_tracker.py
+++ page/meeting_tracker.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-from MeetingManagement.servies.meeting_service import extract_audio_from_video, get_text_from_audio
+from MeetingManagement.servies.meeting_service import extract_audio_from_video, analyze_meeting
 
 def run():
 
@@ -23,10 +23,44 @@ def run():
 
         is_audio_available = extract_audio_from_video(video_file_path)
 
-        get_text_from_audio(os.path.join(VIDEO_DIR, "audio.mp3"))
-
         if is_audio_available:
             st.success("Video file uploaded successfully")
+
+            clicked = st.button("Analyze Meeting")
+
+            if clicked:
+
+                response = analyze_meeting()
+
+                if len(response) == 0:
+                    st.markdown(
+                        """
+                        <div style='background-color:#f8d7da;padding:10px;border-radius:5px;'>
+                            <strong style='color:#721c24;'>All the points are covered in the meeting.</strong>
+                        </div>
+                        """, unsafe_allow_html=True
+                    )
+
+                else:
+                    st.markdown(
+                        """
+                        <div style='background-color:#fff3cd;padding:10px;border-radius:5px;'>
+                            <strong style='color:#856404;'>The following points are not covered in the meeting:</strong>
+                        </div>
+                        """, unsafe_allow_html=True
+                    )
+                    
+                    for point in response:
+                        st.markdown(
+                            f"""
+                            <div style='background-color:#e2e3e5;padding:10px;margin-top:5px;border-radius:5px;'>
+                                <span style='color:#383d41;'>{point}</span>
+                            </div>
+                            """, unsafe_allow_html=True
+                        )
+            else:
+                st.info("Click on the 'Analyze Meeting' button to analyze the meeting")
+
         else:
             st.warning("Audio is not available in the video file")
 
--- src/MeetingManagement/servies/meeting_service.py
+++ src/MeetingManagement/servies/meeting_service.py
@@ -3,6 +3,11 @@ import os
 from transformers import pipeline
 from MeetingManagement import logger
 import soundfile as sf 
+from langchain_core.documents import Document
+from langchain.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from uuid import uuid4
+import json
 
 ## Define the input video file path and output audio file path
 
@@ -42,8 +47,68 @@ def get_text_from_audio(audio_file_path):
         if text is not None:
             with open(transcript_path, "w") as file:
                 file.write(text)
+
+        return text
         
     except Exception as e:
         logger.info("Error in extracting text from audio: ", e)
         raise e
+    
+def split_document_content(text, words_per_chunk = 50):
+
+    words = text.split()
+
+    text_chunks = []
+
+    for i in range(0, len(text), words_per_chunk):
+        text_chunks.append(' '.join(words[i:i+words_per_chunk]))
+
+    documents = [Document(page_content = content) for content in text_chunks]
+
+    return documents
+
+
+def intialize_vector_stores(documents):
+    
+    embeddings = HuggingFaceEmbeddings()
+
+    db = Chroma(embedding_function = embeddings)
+
+    uuids = [str(uuid4()) for _ in range(len(documents))]
+    db.add_documents(documents)
+
+    return db
+
+def get_undiscussed_point(discussion_point, vector_store):
+
+    undiscussed_point = []
+
+    for point in discussion_point:
+
+        similar_docs = vector_store.similarity_search_with_relevance_scores(point, score_threshold = 0.2)    
+
+        if len(similar_docs) == 0:
+            undiscussed_point.append(point)
+
+    return undiscussed_point
+    
+def analyze_meeting():
+
+    audio_file_path = os.path.join("database/meeting_tracking", "audio.mp3")
+    discussion_points_path = "database/discussion_points/discussion_points.json"
+
+    with open(discussion_points_path, "r") as file:
+        discussion_points = json.load(file)
+
+    transcript = get_text_from_audio(audio_file_path)
+
+    documents = split_document_content(transcript)
+
+    vector_store = intialize_vector_stores(documents = documents)
+
+    response = get_undiscussed_point(discussion_point = discussion_points, vector_store = vector_store)
+
+    return response
+
+
 
