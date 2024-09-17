124402b6e3074fd2e46ce1480f1b9de7b92e3250
Tapan Kheni <tapankheni@Tapans-MacBook-Air.local>
2024-09-17 18:24:08 +0530

    init_setup.sh file added for initial project setup

---
+++ init_setup.sh
@@ -0,0 +1,20 @@
+echo[$(date)] : "Setting up the intial setup for the project"
+
+echo[$(date)] : "Creating the virtual environment with python version 3.9.6"
+
+python3 -m venv venv
+
+echo[$(date)] : "Activating the virtual environment"
+
+source venv/bin/activate
+
+echo[$(date)] : "Upgrading pip setuptools and wheel"
+
+pip install --upgrade pip setuptools wheel
+
+echo[$(date)] : "Installing the requirements"
+
+pip install -r requirements.txt
+
+echo[$(date)] : "Initial setup completed"
+
