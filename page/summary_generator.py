146f079ac7810708d5725bbf32e357fe4853e07d
Tapan Kheni <tapankheni@Tapans-MacBook-Air.local>
2024-09-17 22:28:04 +0530

    Summary generator component modified

--- page/summary_generator.py
+++ page/summary_generator.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from MeetingManagement.servies.summary_service import get_summary
 
+# main function to run the streamlit code for summary generator page
 def run():
 
+    # title and description of the page
     st.header("Summary Generator")
-
     st.write('This is a summary generator tool that generates a summary of the meeting. You can use this tool to generate a summary of the meeting and share it with your team members who have not attended the meeting.')
 
     is_clicked = st.button("Get Summary")
 
+    # check if the button is clicked or not
     if is_clicked:
+        # get the summary of the meeting
         summary_text = get_summary()
         st.write(summary_text)
     else:
--- src/MeetingManagement/servies/summary_service.py
+++ src/MeetingManagement/servies/summary_service.py
@@ -1,11 +1,15 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 import google.generativeai as genai
+
+from MeetingManagement import logger
 
 load_dotenv()
 
+# Configure the API key
 genai.configure(api_key = os.getenv('GEMINI_API_KEY'))
 
+# Prompt for the summary generation
 prompt = """You are a project manager at a company. You have a meeting with your team to discuss the some of the important points. Now your task is to create a summary to give it to your other team members who haven't attended the meeting. Write a summary based on the transcript given to you. The summary should include the main points like: 
 - What was discussed during the meeting.
 - Key decisions made during the meeting if it's available directly in the transcript otherwise just avoid it instead of fabricating it.
@@ -22,16 +26,28 @@ Ensure clarity by using bullet points or short paragraphs for easy reading.
 Here is the transcript: 
 """
 
-transcript_file_path = "database/meeting_tracking/transcript.txt"
+TRANSCRIPT_FILE_PATH = "database/meeting_tracking/transcript.txt"
 
-def get_summary():
+def get_summary() -> str:
+    """
+    Generate a summary of the meeting using the transcript.
 
-    with open(transcript_file_path, 'r') as file:
+    Returns:
+        str: Summary of the meeting.
+    """
+
+    logger.info("Generating summary of the meeting from the transcript...")
+
+    # Read the transcript from the file
+    with open(TRANSCRIPT_FILE_PATH, 'r') as file:
         transcript = file.read()
 
+    # Generate the summary using the Gemini Pro model
     model = genai.GenerativeModel('gemini-pro')
 
     response = model.generate_content(prompt + transcript)
+
+    logger.info("Summary generated successfully.")
 
     return response.text
     
